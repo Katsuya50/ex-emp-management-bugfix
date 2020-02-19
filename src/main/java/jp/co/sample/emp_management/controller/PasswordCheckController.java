c5151c7782a3cbb373c0e092815427bd14d6dcf7
Katsuya50 <katsuya.fujishima@rakus-partners.co.jp>
2020-02-19 16:20:17 +0900

    (1-5)JavaDocの追加

--- src/main/java/jp/co/sample/emp_management/controller/PasswordCheckController.java
+++ src/main/java/jp/co/sample/emp_management/controller/PasswordCheckController.java
@@ -7,10 +7,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 確認用パスワードをチェックするレストコントローラー.
+ * 
+ * @author katsuya.fujishima
+ *
+ */
 @RestController
 @RequestMapping("/check-password")
 public class PasswordCheckController {
 	
+	/**
+	 * 確認用パスワードがパスワードと一致していなければエラーメッセージを返すメソッド.
+	 * 
+	 * @param password パスワード
+	 * @param confirmPassword 確認用パスワード
+	 * @return エラーメッセージ
+	 */
 	@RequestMapping(value = "", method = RequestMethod.POST)
 	public Map<String, String> passwordcheck(String password, String confirmPassword){
 		
